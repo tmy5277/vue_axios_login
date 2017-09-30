8e6f872b9ef1b4299dbf0f8bbb6fb251575e5a04
tmy5277 <1223688937@qq.com>
2017-09-30 15:34:15 +0800

    Revert "done with bug of npm run dev"
    This reverts commit 2c4599f0d872ec4953b491b64f5888cc3be9ee4b.

--- src/App.vue
+++ src/App.vue
@@ -41,60 +41,22 @@ export default {
     logout(){
       this.$store.commit(types.LOGOUT);
       this.$router.push(
-        {
+        [
           path:'/',
-        }
+        ]
         );
     }
   },
 }
 </script>
 
-<style lang="scss">
-    @import "../node_modules/vue-material/dist/vue-material.css";
-    @import "assets/css.css";
-    @import "assets/icon.css";
-
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-
-    .container {
-        width: 1000px;
-        margin: 0 auto;
-    }
-    .left {
-        float: left;
-        text-align: left;
-    }
-    .right{
-        float: right;
-    }
-
-    .md-toolbar .title {
-        vertical-align: text-top;
-        margin-left: 5px;
-    }
-
-    h1, h2 {
-        font-weight: normal;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
-    }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
