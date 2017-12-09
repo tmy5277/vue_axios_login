69aa5d0ffe4a57529cbd870ecd76a2a09592d59c
tmy5277 <1223688937@qq.com>
2017-12-09 17:48:30 +0800

    初始样式+index页面

---
+++ src/components/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ul>
+            <router-link :to="{path:'/repository'}">
+              <md-button href="#/repository"  class="md-raised md-primary">view your repositories</md-button>
+            </router-link>
+        </ul>
+    </div>
+</template>
+
+<script type="application/ecmascript">
+    export default {
+        data () {
+            return {
+                msg: ''
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss' rel="stylesheet/scss" type="text/css">
+
+</style>
